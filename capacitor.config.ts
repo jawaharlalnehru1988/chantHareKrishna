e5512641fb815a63433aaf93003ee26d405926e2
nehru00123@gmail.com <nehru00123@gmail.com>
2025-08-25 08:21:10 +0530

    Add Capacitor Android and iOS platforms for mobile app builds

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,29 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'chantHareKrishna',
-  webDir: 'www'
+  appId: 'com.chant.harekrishna',
+  appName: 'Hare Krishna Chanting',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    }
+  }
 };
 
 export default config;
